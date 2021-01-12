d47208a438ff1e5f27539c7556a2ef300a0d04c1
Sara Cantero <cantero.mesar20@cadiz.salesianos.edu>
2021-01-12 09:47:56 +0100

    PrimerCommit

---
+++ PrivadoSeg/src/main/java/com/example/demo/PrivadoSegApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrivadoSegApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrivadoSegApplication.class, args);
+	}
+
+}
